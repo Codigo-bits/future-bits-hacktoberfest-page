3ee72a45fa6fcd7f6a8e35e2541e5947802d689e
DARWINDEV <darwinjh.dev@gmail.com>
2021-10-20 14:50:37 -0500

    directorio js en assets

--- js/index.js
+++
@@ -1,8 +0,0 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navItems = document.querySelector('.nav-items');
-
-navToggle.addEventListener("click", () => {
-    navItems.classList.toggle("nav-items_visible");
-
-    (navItems.classList.contains("nav-menu_visible")) ? navToggle.setAttribute("arial-lable", "Close menu") : navToggle.setAttribute("arial-lable", "Open menu")
-});
